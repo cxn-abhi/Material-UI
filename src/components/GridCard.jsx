b295037197dbe05ba005d200daa5cf8352edef5c
cxn-abhi <150309106+cxn-abhi@users.noreply.github.com>
2024-01-04 11:43:34 +0530

    Update GridCard.jsx

--- src/components/GridCard.jsx
+++ src/components/GridCard.jsx
@@ -1,1 +1,103 @@
+import {
+  Box,
+  Stack,
+  Grid,
+  Paper,
+  Card,
+  CardContent,
+  CardActions,
+  CardMedia,
+  Typography,
+  Button,
+} from "@mui/material";
+import Container from "@mui/material/Container";
+import imageUrl from "./image1.jpg";
+export const GridCard = () => {
+  return (
+    <>
+      <Container maxWidth="lg" style={{ marginTop: "20px" }}>
+        <Typography variant="h4" style={{ textAlign: "center" }}>
+          Responsive Cards
+        </Typography>
+        <Grid container>
+          <Grid item xs={12} md={6} lg={4}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="Image 1"
+                height="140"
+                image={imageUrl}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Card1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={4} lg={4}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image={imageUrl}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Card2
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={4} lg={4}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image={imageUrl}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Crad3
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+};
 
